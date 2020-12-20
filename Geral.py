0cd8dab85b1f4ed0f05670b953c8e4da29095d28
AkiraDemenech <guiakira.demenechmori@gmail.com>
2020-12-19 23:41:42 -0300

    Adição do poema dadadadada....daísta
    Por padrão só é feita uma iteração do sorteio do vocabulário do poema, mas é possível dizer qual o máximo de repetições dada e a probabilidade de escapar antes desse teto.
    
    Agora sim a versão geral de python faz o mesmo que a geral em java

--- Geral.py
+++ Geral.py
@@ -1,34 +1,48 @@
 import random
 
-def dadapo (versos, vocabu = None, prefixo = '', sep = ' ', tab = True):
+def dadapo (versos, vocabu = None, prefixo = '', sep = ' ', tab = True, max = 1, esc = 0):
 	if vocabu == None:
 		vocabu = palavras(versos)
-
+		
 	if versos.__class__ == str:
 		versos = versos.splitlines()
 
-	ddp = []
-	c = len(versos)
-	while c > 0:
-		c += -1
-		ln = prefixo
-		if tab:
-			d = 0
-			while d < len(versos[c]):
-				if not versos[c][d].isspace():
-					ln += versos[c][:d]
-					break
-				d += 1#versos[c][d].isspace()
-		while len(ln) < len(versos[c]):
-			ln += vocabu[int(random.random()*len(vocabu))] + sep
-		ddp.insert(0,ln)
+	try:
+		ddp = []
+		while max > 0:
+			max -= 1	
+			c = len(versos)
+			ddp.clear()
+			while c > 0:
+				c += -1
+				ln = prefixo
+				if tab:
+					d = 0
+					while d < len(versos[c]):
+						if not versos[c][d].isspace():
+							ln += versos[c][:d]
+							break
+						d += 1#versos[c][d].isspace()
+				while len(ln) < len(versos[c]):
+					ln += vocabu[int(random.random()*len(vocabu))] + sep
+				ddp.insert(0,ln)
+			vocabu.clear()
+			vocabu = palavras(ddp,vocabu)
+			if esc > random.random():
+		#		print('Fim prematuro')
+				break
+		#	print(max)
+	except KeyboardInterrupt:
+		print('\nRetornando resultado atual')
 	return ddp
 	
-def palavras (fonte):
+def palavras (fonte, res = None):
 	if fonte.__class__ == str:
 		return fonte.split()
+	
+	if res == None:
+		res = []
 		
-	res = []
 	for s in fonte:
 		res.extend(palavras(s))
 	return res
